b0eb7e633c613c02a4871dfe01656f1763333ff9
Talibiq123 <talibsaeed90@gmail.com>
2024-11-21 17:51:38 +0530

    setting up logo....

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={logo} alt="" />
+      <img className='logo' src={logo} alt="logo" />
       <ul className='navbar-menu'>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>About Me</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact Me</li>
+        <li>Blogs</li>
       </ul>
     </div>
   )
