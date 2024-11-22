a9dcfae5154ac4d028daf0852019139837734ef1
Talibiq123 <talibsaeed90@gmail.com>
2024-11-22 20:36:01 +0530

    Navbar...

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <img className='logo' src={logo} alt="logo" />
-      <ul className='navbar-menu'>
-        <li>About Me</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact Me</li>
-        <li>Blogs</li>
-      </ul>
+    <div>
+      <div className="navbar">
+        <img className="logo" src={logo} alt="logo" />
+        <ul className="navbar-menu">
+          <li>About Me</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact Me</li>
+          <li>Blogs</li>
+        </ul>
+      </div>
+      <div class="main">
+        <h3>Hello, Everyone. I am </h3>
+        <h1>MOHD TALIB</h1>
+        <h3>Web Developer</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
